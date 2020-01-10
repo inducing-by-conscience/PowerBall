d335f1bbd69faf03b9ba4f438641e2f513f85a8c
inducing-by-conscience <inducingbyconscience@gmail.com>
2020-01-09 21:20:29 -0800

    Update method to format the display of the result.

--- src/PowerBall.java
+++ src/PowerBall.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
+
 /**
  * @author jinjiantan
  * @version 0.1
@@ -15,18 +16,22 @@ public class PowerBall {
         System.out.println("How many plays do you want to get?");
         Scanner sysIn = new Scanner(System.in);
         int plays = sysIn.nextInt();
+        //playResult is the multi-dimension arrays to hold the results.
         int[][] playsResult = new int[plays][6];
-        while (plays > 0) {
-            playGenerator(playsResult[plays - 1]);
-            plays--;
+        int playsCounter = plays;
+        while (playsCounter > 0) {
+            playGenerator(playsResult[playsCounter - 1]);
+            playsCounter--;
         }
-        System.out.println("These are what we get for you.The last one is the PowerBall. Good Luck!");
-        System.out.println(Arrays.deepToString(playsResult));
+        System.out.println("These are what we get for you.The last one is the PowerBall:");
+        resultFormatter(playsResult, plays);
+        //System.out.println(Arrays.deepToString(playsResult));
     }
 
     public static void playGenerator(int[] plays) {
         Random generator = new Random();
         int whiteBallCounter = 0;
+        // Generate one set of white ball, put them in the place with the index from 0 to 4
         while (whiteBallCounter < 5) {
             int whiteBall = generator.nextInt(69) + 1;
             if (!conatains(plays, whiteBall)) {
@@ -34,7 +39,10 @@ public class PowerBall {
                 whiteBallCounter++;
             }
         }
+        //generate the power ball, and put it into the place with index 5
         plays[whiteBallCounter] = generator.nextInt(26) + 1;
+        //sort the white balls.
+        Arrays.sort(plays,0,5);
     }
 
     public static boolean conatains(int[] array, int element) {
@@ -45,5 +53,16 @@ public class PowerBall {
             }
         }
         return result;
+    }
+
+    public static void resultFormatter(int[][] playsResult, int plays){
+        for ( int playsCounter = 0; playsCounter < plays; playsCounter++ ){
+            System.out.print("whiter balls [ ");
+            for( int counter = 0; counter < 5; counter ++ ){
+                System.out.print(playsResult[playsCounter][ counter ] + (counter == 4 ? "":", "));
+            }
+            System.out.println(" ] power ball: [ " + playsResult[playsCounter][5] + " ].");
+        }
+        System.out.println("Good Luck! :)");
     }
 }
